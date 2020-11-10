5000b848207fffdff89a53bd639f6c3201018054
andress20 <andress20@gmail.com>
2020-11-09 20:33:26 -0500

    Create style header

--- alamesa/src/components/Header.js
+++ alamesa/src/components/Header.js
@@ -1,11 +1,34 @@
 import Logo from '../image/Logo.png'
+import styled from 'styled-components'
+
+const ContainerHeader = styled.header`
+	display: grid;
+	box-shadow: 0px 0px 4px 4px #707070;
+	background-color: #f8f8f8;
+	grid-template-columns: 30% 70%;
+	grid-template-rows: 60px;
+`
+const HeaderHome = styled.div`
+	display: flex;
+	width: 40vw;
+`
+const Img = styled.img`
+	width: 50px;
+`
+const NavigationMenu = styled.ul`
+	display: flex;
+	list-style: none;
+	justify-content: space-between;
+`
+
 
 function Header(){
+
 	return (
-		<header className = 'header'>
-			<div className = 'header__home'>
+		<ContainerHeader>
+			<HeaderHome>
 				<figure className = 'header__home-logo'>
-					<img 
+					<Img 
 						src = { Logo }
 						alt = 'Logo'
 					/>
@@ -13,9 +36,9 @@ function Header(){
 				<h1 className = 'header__home-slogan'>
 					# Alamesa
 				</h1>
-			</div>
+			</HeaderHome>
 			<nav className = 'navigation'>
-				<ul className = 'navigation__menu'>
+				<NavigationMenu>
 					<li className = 'navigation__menu-item restaurant' >
 						<a href = './'>Restaurantes</a>
 					</li>
@@ -25,9 +48,9 @@ function Header(){
 					<li className = 'navigation__menu-item sign-up' >
 						<a href = './'>Crear Cuenta</a>
 					</li>
-				</ul>
+				</NavigationMenu>
 			</nav>
-		</header>
+		</ContainerHeader>
 	)
 }
 
