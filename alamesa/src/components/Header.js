631bc025ec5924049cafcf15b901f0fdd266a4b2
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-11-10 12:22:16 -0500

    add more styles for header

--- alamesa/src/components/Header.js
+++ alamesa/src/components/Header.js
@@ -1,55 +1,93 @@
 import Logo from '../image/Logo.png'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const ContainerHeader = styled.header`
-	display: grid;
+	display: flex;
 	box-shadow: 0px 0px 4px 4px #707070;
 	background-color: #f8f8f8;
-	grid-template-columns: 30% 70%;
-	grid-template-rows: 60px;
-`
+	justify-content: space-between;
+`;
 const HeaderHome = styled.div`
 	display: flex;
 	width: 40vw;
-`
+	margin: 0 40px
+`;
 const Img = styled.img`
 	width: 50px;
-`
+`;
+const HeaderHomeLogo = styled.figure `
+	margin: 5px;
+`; 
+const HeaderHomeSlogan = styled.h1 `
+	margin: 5px;
+`;
+
 const NavigationMenu = styled.ul`
 	display: flex;
 	list-style: none;
 	justify-content: space-between;
-`
+`;
 
+const NavigationMenuItem = styled.li `
+	margin-right: 17px;
+`;
+
+const Navigation = styled.nav ` 
+	margin-right: 40px;
+	padding: 5px 0;
+`;
+
+const Anchor = styled.a ` 
+	text-decoration: none;
+	color: black;
+	font-size: 18px;
+`;
+
+const AnchorSingIn = styled(Anchor) ` 
+	background-color: #7741FF;
+	padding: 5px 20px;
+	border-radius: 4px;
+	color: white;
+`;
+
+const AnchorSingUp = styled(Anchor) ` 
+	background-color: #0F31DD;
+	padding: 5px 20px;
+	border-radius: 4px;
+	color: white;
+`;
 
 function Header(){
-
 	return (
 		<ContainerHeader>
 			<HeaderHome>
-				<figure className = 'header__home-logo'>
+				<HeaderHomeLogo className = 'header__home-logo'>
 					<Img 
 						src = { Logo }
 						alt = 'Logo'
 					/>
-				</figure>
-				<h1 className = 'header__home-slogan'>
+				</HeaderHomeLogo>
+				<HeaderHomeSlogan className = 'header__home-slogan'>
 					# Alamesa
-				</h1>
+				</HeaderHomeSlogan>
 			</HeaderHome>
-			<nav className = 'navigation'>
+			<Navigation className = 'navigation'>
 				<NavigationMenu>
-					<li className = 'navigation__menu-item restaurant' >
-						<a href = './'>Restaurantes</a>
-					</li>
-					<li className = 'navigation__menu-item sign-in' >
-						<a href = './'>Iniciar Sesion</a>
-					</li>
-					<li className = 'navigation__menu-item sign-up' >
-						<a href = './'>Crear Cuenta</a>
-					</li>
+					<NavigationMenuItem className = 'navigation__menu-item restaurant' >
+						<Anchor
+						 href= './'>Restaurantes</Anchor>
+					</NavigationMenuItem>
+					<NavigationMenuItem className = 'navigation__menu-item sign-in' >
+						<AnchorSingIn
+						 href= './'>Iniciar Sesion</AnchorSingIn>
+					</NavigationMenuItem>
+					<NavigationMenuItem className = 'navigation__menu-item sign-up' >
+						<AnchorSingUp
+						 href= './'>Crear Cuenta</AnchorSingUp>
+					</NavigationMenuItem>
 				</NavigationMenu>
-			</nav>
+			</Navigation>
 		</ContainerHeader>
 	)
 }
