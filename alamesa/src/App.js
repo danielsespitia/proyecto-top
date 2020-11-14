df4d1ab8a3817d2dd21fd33a970f7afa8498caa8
danielsespitia <63252057+danielsespitia@users.noreply.github.com>
2020-11-14 12:49:57 -0500

    Feature rpf001/002 (#6)
    * create restaurantprofile form
    
    * Create Restaurant Profile Form holding data
    
    * correct code writing
    
    * correct identation in VSCode
    
    * changed name to components and deleted console logs
    
    * Update App.js

--- alamesa/src/App.js
+++ alamesa/src/App.js
@@ -8,6 +8,8 @@ from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import FormSignUp from './pages/FormSignUp'
+import RestaurantForm from './pages/RestaurantForm'
+import RestaurantProfile from './pages/RestaurantProfile'
 import Restaurants from './pages/Restaurants'
 
 function App () {
@@ -17,6 +19,10 @@ function App () {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/sign-up" component={FormSignUp}/>
+        <Route
+          exact path='/restaurant-profile' 
+          component={RestaurantForm}/>
+        <Route path="/your-profile" component={RestaurantProfile}/>
         <Route exact path="/restaurants" component={Restaurants}/>
       </Switch>
     </Router>
