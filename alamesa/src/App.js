53a9c038258c4b316fe73f32cce59918ef76bf67
Carolina Salazar <65572735+carosalazar28@users.noreply.github.com>
2020-11-14 18:02:06 -0500

    Feature log001 form login (#8)
    * create form for sign in like a page
    
    * add botton and styles for it, aditional add anchor label for create account
    
    * change font family for input to montserrat alternates
    
    * create function for validated the inputs, add package uuid for mock data
    
    * Add const data for the validation with mock data and form
    
    * change logical for validated
    
    * add message when the user click iniciar sesion

--- alamesa/src/App.js
+++ alamesa/src/App.js
@@ -8,6 +8,7 @@ from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import FormSignUp from './pages/FormSignUp'
+import FormSignIn from './pages/FormSignIn'
 import RestaurantForm from './pages/RestaurantForm'
 import RestaurantProfile from './pages/RestaurantProfile'
 import Restaurants from './pages/Restaurants'
@@ -19,9 +20,8 @@ function App () {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/sign-up" component={FormSignUp}/>
-        <Route
-          exact path='/restaurant-profile' 
-          component={RestaurantForm}/>
+        <Route exact path="/sign-in" component={FormSignIn}/>
+        <Route exact path='/restaurant-profile' component={RestaurantForm}/>
         <Route path="/your-profile" component={RestaurantProfile}/>
         <Route exact path="/restaurants" component={Restaurants}/>
       </Switch>
