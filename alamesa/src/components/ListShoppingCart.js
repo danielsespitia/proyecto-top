1b8429e22457986a0546a9c7f54441352fff9022
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-11-17 21:54:28 -0500

    add style for container title shopping and container listshopping

--- alamesa/src/components/ListShoppingCart.js
+++ alamesa/src/components/ListShoppingCart.js
@@ -1,20 +1,41 @@
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import ContainerContent from './styled/ContainerContent'
+import ButtonPrimary from './styled/ButtonPrimary'
+
+const ContainerTitleShopping = styled.span`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  border-bottom: 1px solid black;
+  margin: auto;
+`
+const TextStrongTotal = styled.td`
+  font-weight: 800;
+`
 
 export function ListShoppingCart () {
   return(
     <section>
-      <span>
-        <h1>Mi carrito</h1>
+      <ContainerTitleShopping>
+        <h2>Mi carrito</h2>
         <FontAwesomeIcon icon={faShoppingCart}/>
-        <hr></hr>
-      </span>
-      <table>
-        <tr>
-          <td>Pago minimo</td>
-          <td>$48.000</td>
-        </tr>
-      </table>
+      </ContainerTitleShopping>
+      <ContainerContent>
+        <table>
+          <tr>
+            <td>Pago minimo</td>
+            <td>$48.000</td>
+          </tr>
+          <tr>
+            <TextStrongTotal>Total</TextStrongTotal>
+            <td>$48.000</td>
+          </tr>
+          </table>
+        <ButtonPrimary as="button">Finalizar tu orden</ButtonPrimary>
+      </ContainerContent>
     </section>
 
   )
