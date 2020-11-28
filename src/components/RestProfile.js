55f195c8bf05bdfb7af663cbdc6c5eb170767dcc
Daniel Espitia <dsespitiac@gmail.com>
2020-11-28 17:35:35 -0500

    modified restaurant profile component added style

--- src/components/RestProfile.js
+++ src/components/RestProfile.js
@@ -1,28 +1,192 @@
 import styled from 'styled-components';
+import ButtonPrimary from '../components/styled/ButtonPrimary';
 
 const Container = styled.div `
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 20% 80%;
   grid-gap: 20px 60px;
   text-align: left;
 `;
 
-const DataItem = styled.div`
+const SignUpData = styled.div`
   display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const DataItem = styled.div`
   grid-row-gap: 5px;
 `;
 
-function RestProfile ( { restaurantName, address, email, phone, scheduleFrom, scheduleTo, deposit, nit } ) {
+const Form = styled.form `
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px 0px;
+`;
+
+const Label = styled.label `
+  display: block;
+`;
+
+const Input = styled.input ` 
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #CED4DA;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const ContentButtons = styled.div`
+  grid-column: 1/3;
+  display: flex;
+  justify-content: center;
+  padding-block-start: 40px;
+`;
+
+const ButtonUpdate = styled(ButtonPrimary)`
+  margin-right: 50px;
+`;
+
+const ButtonCancel = styled(ButtonPrimary)`
+  background-color: ${
+    props => props.theme.tertiaryColor
+  };
+  &:hover {
+    background-color: #E2DE5B;
+    border: 1px solid ${
+      props => props.theme.tertiaryColor
+    };
+  }
+  margin-left: 50px;
+`;
+
+function RestProfile ({ 
+  restaurantName, 
+  address,
+  email,
+  phone,
+  scheduleFrom,
+  scheduleTo,
+  deposit,
+  nit,
+  handleChange,
+  handleSubmit,
+  handleDeleteClient,
+})
+{
   return (
     <Container>
-      <DataItem><strong>Nombre:</strong>{restaurantName}</DataItem>
-      <DataItem><strong>Dirección: </strong> {address}</DataItem>
-      <DataItem><strong>Correo Electrónico:</strong> {email}</DataItem>
-      <DataItem><strong>Teléfono:</strong> {phone}</DataItem>
-      <DataItem><strong>Hora de Apertura:</strong> {scheduleFrom}</DataItem>
-      <DataItem><strong>Hora de Cierre:</strong> {scheduleTo}</DataItem>
-      <DataItem><strong>Pago Mínimo:</strong> ${deposit}</DataItem>
-      <DataItem><strong>NIT:</strong> {nit}</DataItem>
+      <SignUpData>
+        <DataItem>
+          <p><strong>Nombre:</strong></p>
+          <p>{restaurantName}</p></DataItem>
+        <DataItem>
+          <p><strong>Correo Electrónico:</strong></p>
+          <p>{email}</p>
+        </DataItem>
+      </SignUpData>
+      <Form onSubmit={handleSubmit}>
+        <div>
+          <Label><strong>Dirección</strong></Label>
+          <Input
+            className="Form__address-input"
+            id="address"
+            type="text"
+            name="address"
+            value={address}
+            autoComplete="on"
+            onChange={handleChange}
+            placeholder="Ej: Calle 5a #23-43"
+            required
+          />
+        </div>
+        <div>
+          <Label><strong>Número de Teléfono</strong></Label>
+          <Input
+            className="Form__phone-input"
+            id="phone"
+            type="text"
+            name="phone"
+            value={phone}
+            autoComplete="on"
+            onChange={handleChange}
+            placeholder="Ej: 3203212345"
+            required
+          />
+        </div>
+        <div>
+          <Label><strong>Hora de Apertura:</strong></Label>
+          <Input
+            className="Form__restaurantScheduleFrom-input"
+            id="scheduleFrom"
+            type="text"
+            name="name"
+            value={scheduleFrom}
+            autoComplete="on"
+            onChange={handleChange}
+            placeholder="Hora de Apertura"
+            required
+          />
+        </div>
+        <div>
+          <Label><strong>Hora de Cierre</strong></Label>
+          <Input
+            className="Form__scheduleTo-input"
+            id="scheduleTo"
+            type="text"
+            name="scheduleTo"
+            value={scheduleTo}
+            autoComplete="on"
+            onChange={handleChange}
+            placeholder="Hora de Cierre"
+            required
+          />
+        </div>
+        <div>
+          <Label><strong>NIT</strong></Label>
+          <Input
+            className="Form__nit-input"
+            id="nit"
+            type="text"
+            name="nit"
+            value={nit}
+            autoComplete="on"
+            onChange={handleChange}
+            placeholder="Ingresa tu NIT"
+            required
+          />
+        </div>
+        <div>
+          <Label><strong>Pago Mínimo</strong></Label>
+          <Input
+            className="Form__deposit-input"
+            id="deposit"
+            type="number"
+            name="deposit"
+            value={deposit}
+            autoComplete="on"
+            onChange={handleChange}
+            placeholder="Mínimo $20.000"
+            required
+          />
+        </div>
+        <ContentButtons>
+          <ButtonUpdate
+            className="ButtonUpdate"
+            id="ButtonUpdate"
+            type="submit"
+            value="Actualizar"              
+          >
+          </ButtonUpdate>
+          <ButtonCancel
+            className="ButtonCancel"
+            id="ButtonCancel"
+            type="button"
+            value="Eliminar Cuenta"
+            onClick={handleDeleteClient}
+          >
+          </ButtonCancel>
+        </ContentButtons>
+      </Form>
     </Container>
   )
 }
