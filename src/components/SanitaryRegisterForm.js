ab47df7c280fd5c289d788cb0d6e74e425e333c4
Sergio Jaramillo <sergiojaramilloluna@gmail.com>
2020-11-28 15:11:38 -0500

    divided SAN001 in components

---
+++ src/components/SanitaryRegisterForm.js
@@ -0,0 +1,166 @@
+import styled from 'styled-components'
+import ContainerContent from './styled/ContainerContent'
+import ButtonPrimary from './styled/ButtonPrimary'
+
+const ContainerList = styled(ContainerContent)`
+
+`;
+
+const Form = styled.form`
+  display: grid;
+  justify-content: center;
+  grid-row-gap: 10px;
+`;
+
+const H3 = styled.h3`
+  text-align: center;
+`;
+
+const H4 = styled.h4`
+  font-size: 14px;
+`;
+
+const Paragraph = styled.p`
+  font-size: 16px;
+  color: #2F80ED
+`;
+
+const Span = styled.span ` 
+  padding: 10px;
+  background-color: ${
+    props => props.theme.blueColorSan
+  };
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 10px;  
+`;
+
+const Checkbox = styled.input`
+  border: 1px solid #CED4DA;
+  box-sizing: border-box;
+  border-radius: 2px;
+`;
+
+const Temperature = styled.input`
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #CED4DA;
+  border-color: ${
+    props => props.theme.grayColorMore
+  };
+`;
+
+const ContentButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ButtonCancel = styled(ButtonPrimary)`
+  background-color: ${
+    props => props.theme.tertiaryColor
+  };
+
+  &:hover {
+      background-color: #E2DE5B;
+      border: 1px solid ${
+        props => props.theme.tertiaryColor
+      };
+  }
+`;
+
+export function SanitaryRegisterForm({
+  question1SymptomsCovid,
+  question2ContactWithPeople,
+  question3InternationalTravel,
+  question4HealthWorker,
+  temperature,
+  handleChange,
+  handleSubmit,
+  handleCancel,
+})
+{
+  return(
+    <ContainerList>
+      <H3>Registro Sanitario</H3>
+      <H4>Selecciona los recuadros según tu estado:</H4>
+      <Form onSubmit={handleSubmit}>
+        <Span className="ContentQuestion">
+          <Paragraph>He presentado sintomas relacionados con Covid19</Paragraph>
+          <Checkbox
+            className="response"
+            id="question1SymptomsCovid"
+            type="checkbox"
+            name="question1SymptomsCovid"
+            value={question1SymptomsCovid}
+            onChange={handleChange}
+          >
+          </Checkbox>
+        </Span>
+        <Span className="ContentQuestion">
+          <Paragraph>He estado en contacto con personas con sintomas de Covid19</Paragraph>
+          <Checkbox
+            className="response"
+            id="question2ContactWithPeople"
+            type="checkbox"
+            name="question2ContactWithPeople"
+            value={question2ContactWithPeople}
+            onChange={handleChange}
+          >
+          </Checkbox>
+        </Span>
+        <Span className="ContentQuestion"> 
+          <Paragraph>Hice un viaje internacional en los ultimos 30 días</Paragraph>
+          <Checkbox
+            className="response"
+            id="question3InternationalTravel"
+            type="checkbox"
+            name="question3InternationalTravel"
+            value={question3InternationalTravel}
+            onChange={handleChange}
+          >
+          </Checkbox>
+        </Span>
+        <Span className="ContentQuestion">
+          <Paragraph>Soy trabajador de la salud</Paragraph>
+          <Checkbox
+            className="response"
+            id="question4HealthWorker"
+            type="checkbox"
+            name="question4HealthWorker"
+            value={question4HealthWorker}
+            onChange={handleChange}
+          >
+          </Checkbox>
+        </Span>
+        <Temperature
+          className="Temperature"
+          id="temperature"
+          type="text"
+          name="temperature"
+          value={temperature}
+          onChange={handleChange}
+          placeholder="Cual es mi temperatura"
+        >
+        </Temperature>
+        <ContentButtons>
+          <ButtonPrimary
+            className="ButtonUpdate"
+            id="ButtonUpdate"
+            type="submit"
+            value="Actualizar"
+          >
+          </ButtonPrimary>
+          <ButtonCancel
+            className="ButtonCancel"
+            id="ButtonCancel"
+            type="button"
+            onClick={handleCancel}
+            value="Cancelar"
+          >
+          </ButtonCancel>
+        </ContentButtons>
+      </Form>
+    </ContainerList>
+  )
+}
