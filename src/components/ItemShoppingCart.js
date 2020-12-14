9cea773b654eb1c34da22dbeb87423834de0b892
andress20 <andress20@gmail.com>
2020-12-14 14:57:29 -0500

    Rsv005 reservation payment confirm Component (#64)
    * confirmation payment grafic interface
    
    * separate styles, logic, and presentation
    
    * change button implementation and delete console.log

--- src/components/ItemShoppingCart.js
+++ src/components/ItemShoppingCart.js
@@ -1,7 +1,9 @@
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import ContainerContent from './styled/ContainerContent'
 import ButtonPrimary from './styled/ButtonPrimary'
+import { Link } from 'react-router-dom'
 
 const ContainerTitleShopping = styled.span`
   display: flex;
@@ -26,6 +28,16 @@ const TextStrongTotal = styled.td`
 `;
 
 export function ListShoppingCart ( { deposit } ) {
+  
+  const data = useSelector(
+    ({reservationReducer: {
+      ...state 
+    }}) => {
+    return { ...state }
+  })
+
+  const paymentConfirm = `/restaurants/${data.id}/reservation/confirm`;
+
   return(
     <section>
       <ContainerTitleShopping>
@@ -36,14 +48,14 @@ export function ListShoppingCart ( { deposit } ) {
         <LabelTable>
           <tr>
             <td>Pago minimo</td>
-            <td>{deposit}</td>
+            <td>{data.deposit}</td>
           </tr>
           <tr>
             <TextStrongTotal>Total</TextStrongTotal>
-            <td>{deposit}</td>
+            <td>{data.deposit}</td>
           </tr>
           </LabelTable>
-        <ButtonPrimary as="button">Finalizar tu orden</ButtonPrimary>
+        <ButtonPrimary as={Link} to={paymentConfirm}>Finalizar tu orden</ButtonPrimary>
       </ContainerContentShoppingList>
     </section>
   )
