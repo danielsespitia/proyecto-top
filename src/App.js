a1560747adc61bde7316e91360ffc85724b9d74b
Carolina Salazar <65572735+carosalazar28@users.noreply.github.com>
2020-12-29 17:31:54 -0500

    Feature hdr005 (#77)
    * divided the logic and styles
    
    * created the component modalprofile
    
    * created the dropdown for profile
    
    * fix and add styles to the modal overlay profile
    
    * fix the functions coments
    
    * fix clean code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   Switch,
 } 
 from 'react-router-dom'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import { Footer } from './components/Footer'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
---
+++ src/components/Header/Header.js
@@ -0,0 +1,104 @@
+import { useContext, useState } from 'react';
+import Logo from '../../image/Logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { AuthContext } from '../../store/AuthContext';
+import { Link } from 'react-router-dom';
+import { ModalProfile } from '../Modals/ModalProfile';
+import {
+  ContainerHeader,
+  HeaderHome,
+  Img,
+  HeaderHomeLogo,
+  HeaderHomeSlogan,
+  NavigationMenu,
+  NavigationMenuItem,
+  ContainerNavActions,
+  Navigation,
+  Anchor,
+  AnchorSignIn,
+  AnchorSignUp,
+  ContainerActions,
+  AnchorProfile,
+  AnchorLogout,
+} from './HeaderStyles';
+
+
+
+function Header() {
+  const { isToken, logout, } = useContext(AuthContext);
+  
+  const [modalProfile, setModalProfile] = useState(false);
+
+  const handleClick = () => setModalProfile(true);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setModalProfile(false);
+    } else {
+      setModalProfile(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setModalProfile(false);
+    } else {
+      setModalProfile(false);
+    }
+  };
+
+  return (
+    <ContainerHeader>
+      <HeaderHome to="/">
+        <HeaderHomeLogo className="header__home-logo">
+          <Img src={Logo} alt="Logo" />
+        </HeaderHomeLogo>
+        <HeaderHomeSlogan className="header__home-slogan">
+          # Alamesa
+        </HeaderHomeSlogan>
+      </HeaderHome>
+      <ContainerNavActions>
+        <Navigation className="navigation">
+          <NavigationMenu>
+            <NavigationMenuItem className="navigation__menu-item restaurant">
+              <Anchor to="/restaurants">Restaurantes</Anchor>
+            </NavigationMenuItem>
+          </NavigationMenu>
+        </Navigation>
+        {isToken ? (
+          <>
+            <ContainerActions>
+              <NavigationMenuItem
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                onClick={handleClick}
+                >
+                <AnchorProfile 
+                  as={Link} 
+                >
+                    Profile
+                  <HeaderHomeLogo className="header__home-logo">
+                    <FontAwesomeIcon icon="user-circle" />
+                  </HeaderHomeLogo>
+                </AnchorProfile>
+                {modalProfile && <ModalProfile/>}
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <AnchorLogout as={Link} to="/" onClick={logout}>Cerrar sesión</AnchorLogout>
+              </NavigationMenuItem>
+            </ContainerActions>
+          </>
+        ) : (
+            <>
+              <ContainerActions>
+                <AnchorSignIn as={Link} to="/sign-in">Iniciar sesión</AnchorSignIn>
+                <AnchorSignUp as={Link} to="/sign-up">Crear cuenta</AnchorSignUp>
+              </ContainerActions>
+            </>
+          )}
+      </ContainerNavActions>
+    </ContainerHeader>
+  );
+}
+
+export default Header;
