e954da13091f272ecaf87e506db76adc1730c99a
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-05 10:36:14 -0500

    dispatch function for go to reservation

--- src/components/MiniSearchRestaurant.js
+++ src/components/MiniSearchRestaurant.js
@@ -1,5 +1,11 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import {
+  setRestaurantId,
+  setRestaurantName,
+  setRestaurantDeposit,
+} from '../store/actions/Reservation.actions';
 
 const LinkToRestaurant = styled(Link)`
   font-size: 14px;
@@ -8,13 +14,24 @@ const LinkToRestaurant = styled(Link)`
   };
 `;
 
-function MiniSearchRestaurant({ id, name }) {
+function MiniSearchRestaurant({ id, name, deposit, }) {
+
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(setRestaurantId(id))
+    dispatch(setRestaurantName(name))
+    dispatch(setRestaurantDeposit(deposit))
+  };
+
   return (
     <div>
       <section>
         <LinkToRestaurant to={{
           pathname: `/restaurants/${id}/reservation`,
-        }}>
+        }}
+        onClick={handleClick}
+        >
           <p>{name}</p>
         </LinkToRestaurant>
       </section>
