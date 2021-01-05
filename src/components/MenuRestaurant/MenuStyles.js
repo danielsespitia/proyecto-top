c3f9ad65a9afb1833491a3d1eed6c4a9b3c942ec
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-05 12:30:08 -0500

    created some styles

---
+++ src/components/MenuRestaurant/MenuStyles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import ContainerContent from '../styled/ContainerContent';
+
+export const ContainerAbout = styled(ContainerContent)`
+  margin: revert;
+  position: relative;
+  top: 30px;
+  left: 30px;
+  width: 560px;
+  text-align: center;
+`;
+
+export const BodyLeft = styled(ContainerContent)`
+
+`;
+
+export const BodyRight = styled(ContainerContent)`
+
+`;
