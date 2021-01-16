f93eb363578c5e91c6cb8fa87acb29a42e9e980a
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-16 17:07:19 -0500

    render the component and here is where put the logic

---
+++ src/pages/ResponseEpayco.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import styled from 'styled-components';
+import DesktopStructure from '../components/styled/DesktopStructure';
+import { BodyLeft } from '../components/MenuRestaurant/MenuStyles';
+import ContainerContent from '../components/styled/ContainerContent';
+import { ResponseComponent } from '../components/ResponseEpayco/ResponseReservation';
+
+function queryString(query) {
+  const res = {}
+  query
+    .replace('?', '')
+    .split('&')
+    .forEach(q => {
+      const [key, value] = q.split('=')
+      res[key] = value
+    })
+  return res
+}
+
+const BodyResponse = styled(ContainerContent)`
+  width: fit-content;
+  margin: 0;
+`;
+
+export function Response({ location }) {
+
+  const [responseEpayco, setResponseEpayco] = useState({});
+
+  useEffect(() => {
+    const { ref_payco } = queryString(location.search)
+    axios({
+      method: 'GET',
+      baseURL: process.env.REACT_APP_PAYMENT_URL,
+      url: `/validation/v1/reference/${ref_payco}`
+    })
+      .then(({ data }) => {
+        setResponseEpayco(data)
+      })
+  }, [location])
+
+  return (
+    <DesktopStructure>
+      <BodyLeft>
+        <h4>Ubicación</h4>
+      </BodyLeft>
+      <BodyResponse>
+        <ResponseComponent/>
+      </BodyResponse>
+    </DesktopStructure>
+  )
+};
