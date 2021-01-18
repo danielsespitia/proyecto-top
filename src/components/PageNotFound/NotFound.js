1902583ed36b970e0df6dcf758744d7efabc7c08
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-18 11:52:21 -0500

    created a new component for render a page error

---
+++ src/components/PageNotFound/NotFound.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { ContainerError, ContainerImage } from './NotFoundStyles';
+
+function PageNotFound(props) {
+  return (
+    <>
+      <ContainerError className="Error404___container">
+        <div className="Error404___container-left">
+          <div className="Error404___container-content">
+            <h3>Ha ocurrido algo y no podemos acceder a tu solicitud</h3>
+            <Link to="/" className="btn btn-primary">
+              Regresa al home
+            </Link>
+          </div>
+        </div>
+        <ContainerImage>
+          <img 
+            src="https://res.cloudinary.com/alamesa/image/upload/c_scale,w_1000/v1610980517/UI/2668387_hj0hds.jpg"
+            alt="Imagen render error"
+          />
+          <a href="https://www.freepik.com/vectors/business">Business vector created by pikisuperstar - www.freepik.com</a>
+        </ContainerImage>
+      </ContainerError>
+    </>
+  );
+}
+
+export default PageNotFound
---
+++ src/components/PageNotFound/NotFoundStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ContainerError = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 60px);
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 15px auto;
+  flex-direction: column;
+  justify-content: inherit;
+`;
+
+export const ContainerImage = styled.div`
+  background-repeat: no-repeat;
+  background-position: center;
+`;
