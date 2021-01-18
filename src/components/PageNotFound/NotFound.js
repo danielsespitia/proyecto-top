a4638b6547f919c7861b1373de1caf3423ad068c
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-18 14:49:00 -0500

    add the template

--- src/components/PageNotFound/NotFound.js
+++ src/components/PageNotFound/NotFound.js
@@ -1,27 +1,27 @@
 import { Link } from 'react-router-dom';
-import { ContainerError, ContainerImage } from './NotFoundStyles';
+import { ContainerError, Image, ContainerText, RedirectText } from './NotFoundStyles';
 
 function PageNotFound(props) {
   return (
     <>
       <ContainerError className="Error404___container">
         <div className="Error404___container-left">
-          <div className="Error404___container-content">
+          <ContainerText className="Error404___container-content">
             <h3>Ha ocurrido algo y no podemos acceder a tu solicitud</h3>
-            <Link to="/" className="btn btn-primary">
+            <RedirectText to="/" className="btn btn-primary">
               Regresa al home
-            </Link>
-          </div>
+            </RedirectText>
+          </ContainerText>
         </div>
-        <ContainerImage>
-          <img 
+        <figure>
+          <Image 
             src="https://res.cloudinary.com/alamesa/image/upload/c_scale,w_1000/v1610980517/UI/2668387_hj0hds.jpg"
             alt="Imagen render error"
           />
           <figcaption>
             <a href="https://www.freepik.com/vectors/business">Business vector created by pikisuperstar - www.freepik.com</a>
           </figcaption>
-        </ContainerImage>
+        </figure>
       </ContainerError>
     </>
   );
