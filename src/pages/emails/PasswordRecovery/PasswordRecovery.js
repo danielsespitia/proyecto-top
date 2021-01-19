5da2b325eebcb4f32730391f72995ba5a8c23411
andress20 <andress20@gmail.com>
2021-01-19 14:43:12 -0500

    add password recovery for Restaurant

--- src/pages/emails/PasswordRecovery/PasswordRecovery.js
+++ src/pages/emails/PasswordRecovery/PasswordRecovery.js
@@ -6,13 +6,18 @@ import swal from 'sweetalert'
 function PasswordRecovery() {
 
   const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+  const [userType, setUserType] = useState('clients')
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     switch(name) {
       case 'email':
         setEmail(value)
+        break;
+      case 'userType':
+        setUserType(value)
+        break;
+      default: break;
     }
   };
   
@@ -23,18 +28,18 @@ function PasswordRecovery() {
         method: 'POST',
         baseURL: process.env.REACT_APP_SERVER_URL,
         url: `/email/recovery-pass`,
-        data: { email }
+        data: { email, userType }
       });
       swal('Te hemos enviado un correo de recuperacion de contraseña, por favor revisa tu correo','','success')
     }catch(error){
-      swal('El email que ingresaste no existe en alamesa','','error')
+      swal('El email no existe en alamesa','','error')
     }
   };
 
   return(
     <PasswordRecoveryForm
-      message={message}
       email={email}
+      userType={userType}
       handleChange={handleChange}
       handleSubmit={handleSubmit}
     />
