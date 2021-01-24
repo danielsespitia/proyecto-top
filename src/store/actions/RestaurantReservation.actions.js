c771aab33ab61bfdea2e3b6e9d1cea9d3757ff18
danielsespitia <63252057+danielsespitia@users.noreply.github.com>
2021-01-24 09:38:24 -0500

    Feature rrv005 cancel (#121)
    * installed dependencies
    
    * created new action reducers
    
    * before rebase
    
    * fixed dependencies
    
    * fixed yarn lock

--- src/store/actions/RestaurantReservation.actions.js
+++ src/store/actions/RestaurantReservation.actions.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import swal from 'sweetalert';
+
 import {
     RESERVATION_LOADING,
     RESERVATION_FAILURE,
     RESERVATION_DATA,
     RESERVATION_FINISHED,
+    RESERVATION_DELETE,
 } from '../reducers/RestaurantReservation.reducer';
 
 export function getReservationData (payload) {
@@ -33,3 +36,44 @@ export function getReservationsList() {
         }
     }
 }
+
+export function deleteReservation(reservationId, index) {
+  return async function (dispatch) {
+    const value = await swal("¿Estás seguro que quieres cancelar esta reserva?", {
+      buttons: {
+        regret: "No, no quiero cancelarla",
+        destroy: {
+          text: "Sí",
+          value: "destroy",
+        },
+      },
+    })
+    switch (value) {
+      case "cancel":
+        swal("La reserva sigue en pie");
+        break;
+      case "regret":
+        swal("La reserva no fue cancelada");
+        break;
+      case "destroy":
+        try { 
+          const token = localStorage.getItem('token')
+          await axios({
+            method: 'DELETE',
+            baseURL: process.env.REACT_APP_SERVER_URL,
+            url: `reservations/${reservationId}`,
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          swal("Reserva cancelada", "", "success");
+          dispatch({ type: RESERVATION_DELETE, payload: index})
+        } catch(err) {
+            swal("La reserva no pudo ser cancelada", "", "error");
+        }
+        break;
+      default:
+        swal("La reserva sigue en pie");
+    }
+  };
+};
