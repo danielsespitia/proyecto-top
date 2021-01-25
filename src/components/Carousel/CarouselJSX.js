0c81690d3206ad3db7ad301524636e6b52207873
SergioJaramilloL <70079208+SergioJaramilloL@users.noreply.github.com>
2021-01-24 20:54:32 -0500

    Hom005 (#127)
    * create carousel, but the scroll is missing
    
    * finish carousel
    
    * deleted image withOutLogo

---
+++ src/components/Carousel/CarouselJSX.js
@@ -0,0 +1,124 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import {
+  ContainerFather,
+  TitleCarousel,
+  Title,
+  MainContainer,
+  ButtonLeft,
+  ButtonRight,
+  CarouselContainer,
+  Item,
+  Logo,
+  Name,
+  ButtonVisit
+} from './CarouselStyles';
+
+export function CarouselJSX () {
+
+  const data = useSelector(
+    ({reservationReducer: {
+      ...state
+    }}) => {
+      return { ...state } 
+    })
+    
+  const getWidth = () => window.innerWidth
+  const [state, setState] = useState({
+    activeIndex : 0,
+    translate: 0,
+    transition: 0.45
+  })
+
+  const carousel = {
+    transform: `translateX(-${state.translate}px)`,
+    transition: `transform ease-out ${state.transition}s`,
+    display: 'flex',
+  }
+  const { translate, activeIndex } = state
+
+  const toRight = () => {
+    if ( translate >= getWidth() ) {
+      return setState ({
+        ...state,
+        translate: 0,
+        activeIndex: 0
+      })
+    }
+    setState({
+      ...state,
+      activeIndex: activeIndex + 1,
+      translate: (activeIndex) * (0.2 * getWidth())
+    })
+  }
+
+  const toLeft = () => {
+    if( activeIndex === 0 ) {
+      return setState({
+        ...state,
+        translate: data.restaurantList.length - 1
+      })
+    }
+
+    setState({
+      ...state,
+      activeIndex: activeIndex - 1,
+      translate: (activeIndex - 1) * (0.2 * getWidth())
+    })
+  }
+  
+  return(
+    <>
+      <ContainerFather>
+        <TitleCarousel>
+          <Title>
+            Restaurantes aliados
+          </Title>
+        </TitleCarousel>
+
+        <MainContainer>
+          <ButtonLeft
+            onClick= {toLeft}
+          >
+           <FontAwesomeIcon icon={faAngleLeft} />
+          </ButtonLeft>
+          <CarouselContainer>
+            <div 
+              style={carousel}
+            >
+              {!!data.restaurantList && data.restaurantList.length > 0 && data.restaurantList.map(({ _id, name, logo }) => {
+                return (
+                  <Item>
+                    {!logo ? 
+                    (
+                    <Logo
+                      src= "https://image.freepik.com/vector-gratis/compre-nosotros-somos-senal-abierta_52683-38092.jpg"
+                      alt= "Logo restaurante"
+                    />
+                    ) : (
+                    <Logo
+                      src= {logo}
+                      alt= "Logo restaurante"
+                    />
+                    )}
+                    <Name>
+                      <p>{name}</p>
+                    </Name>
+                    <ButtonVisit>Visitanos</ButtonVisit>
+                  </Item>
+                )
+              })}
+            </div>
+          </CarouselContainer>
+          <ButtonRight
+            onClick= {toRight}
+          >
+            <FontAwesomeIcon icon={faAngleRight} />
+          </ButtonRight>
+        </MainContainer>
+      </ContainerFather>
+    </>
+  )
+}
